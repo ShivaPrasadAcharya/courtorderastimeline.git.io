de05f0f3fd455fa74e012dcee8f7e07954ad5f81
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-01-12 08:21:20 +0545

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-   base: '/exptimeline.git.io/',
+   base: '/courtorderastimeline.git.io/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
